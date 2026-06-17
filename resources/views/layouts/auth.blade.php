000045380b5b403dee11504fa43e3df55c070431
Presione-Enter-by-Jose-Prieto <alejandroprietosal@gmail.com>
2026-06-17 16:19:17 -0500

    Initial commit

---
+++ resources/views/layouts/auth.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es" >
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Jaguar SENA</title>
+    <link rel="icon" href="/favicon.ico" sizes="any">
+    <link rel="icon" href="/favicon.svg" type="image/svg+xml">
+    <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+    @vite('resources/css/app.css')
+    @fonts
+</head>
+<body class="m-0 p-0 h-screen w-screen flex justify-center items-center bg-[url('fondo1.jpg')] bg-cover bg-center">
+    @yield('content')
+</body>
+</html>
